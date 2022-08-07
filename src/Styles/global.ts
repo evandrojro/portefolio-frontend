8651e95d26839c5cdca2a6adc6a4a27e109d6de6
evandrojro <junior.evange@hotmail.com>
2022-08-06 23:17:20 -0300

    Desenvolvido parte de Tema(dark e light) e iniciado componente de Navbar

---
+++ src/Styles/global.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+
+body{
+    background: ${props => props.theme.colors.background};
+    color:${props => props.theme.colors.textPrimary};
+}
+
+
+h1 {
+    font-family: 'DM Sans', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 47px;
+    color:${props => props.theme.colors.textPrimary};
+}
+
+h2 {
+    font-family: 'DM Sans', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 47px;
+    color:${props => props.theme.colors.textPrimary};
+}
+
+
+h3 {
+    font-family: 'DM Sans', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 34px;
+    color:${props => props.theme.colors.textPrimary};
+}
+
+body, button, textarea, input {
+    font-family: ''DM Sans', sans-serif;
+    font-weight: 400;
+  }
+
+html {
+    scroll-behavior: smooth;
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5% // 14px
+    }
+  }
+
+
+`
