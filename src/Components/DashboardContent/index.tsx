449e24490b2f258e19a5bf8886c590b195fc3ea6
evandrojro <junior.evange@hotmail.com>
2022-08-11 22:16:13 -0300

    Criação do conteudo visual do dashboard content

--- src/Components/DashboardContent/index.tsx
+++ src/Components/DashboardContent/index.tsx
@@ -1,11 +1,24 @@
-import { Container } from "./styles";
+import { Button } from "@mui/material";
+import { Container, ContainerButton, Figure, TextSection } from "./styles";
 
 const DashboardContent = () => {
-    return(
+    return (
         <Container>
-
+            <TextSection>
+                <h1 className="">
+                    Olá eu sou o Evandro Jr :)
+                </h1>
+                <span>Desenvolvedor Full-Stack no Havan Labs</span>
+            </TextSection>
+            <ContainerButton>
+                <Button  className="firstButton">Linkedin</Button>
+                <Button className="secondButton">Github</Button>
+            </ContainerButton>
+            <Figure>
+                <img alt="ilustration" src='assets/dashboardimg.svg' />
+            </Figure>
         </Container>
     );
 }
 
-export default DashboardContent();
+export default DashboardContent;
